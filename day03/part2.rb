085b2a966eb05afb3a8470d967e0c59e9e1294de
Tommy Dugger <tommy.dugger78@gmail.com>
2022-12-03 14:16:21 -0700

    Provide second and better solution for part2 with notes

--- day03/part2.rb
+++ day03/part2.rb
@@ -20,3 +20,27 @@ priority_total = File.readlines('input')
     end
 
 pp "Priority total for all badges: #{priority_total}"
+
+# Better solution after discussion with Zack Young.
+# Use Set and intersection to remove N^2.
+#
+# Also, Regex can be slow so using `partition` can
+# be problematic.
+#
+# Providing this as a learning experience. 
+
+priority_score = File.readlines('input')
+    # each_slice iterates for each range of N elements
+    .each_slice(3)
+    .sum do |group|
+        # Turn each elf into a unique new Set.
+        # Also strip whitespace and new line
+        elf1, elf2, elf3 = group.map { |elf| Set.new(elf.strip.chars) }
+        # Find the intersection of elf 1 and 2
+        # and then use that set in an intersection
+        # of elf 3.
+        common_item = elf1.intersection(elf2).intersection(elf3).first
+        alpabet_map.index(common_item) + 1
+    end
+
+pp "Better Solution: Priority total for all sacks: #{priority_score}"
