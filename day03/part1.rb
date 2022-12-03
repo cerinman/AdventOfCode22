70cdaad0690606ab1e0c06a5cc627fa134e9d6ee
Tommy Dugger <tommy.dugger78@gmail.com>
2022-12-03 14:09:26 -0700

    Adding a better solution for reference

--- day03/part1.rb
+++ day03/part1.rb
@@ -19,3 +19,29 @@ priority_total = File.readlines('input')
     end
 
 pp "Priority total for all sacks: #{priority_total}"
+
+# Better solution after discussion with Zack Young.
+# Use Set and intersection to remove N^2.
+#
+# Also, Regex can be slow so using `partition` can
+# be problematic.
+#
+# Providing this as a learning experience. 
+
+priority_score = File.readlines('input')
+    .sum do |elf_sack|
+        # Strip white space and new line
+        elf_sack.strip!
+        # Split the elf_sack into two compartments
+        # String.chars returns an array of characters in the string
+        # each_slice iterates for each range of N elements
+        # Turn each compartment into a unique new Set
+        compartment1, compartment2 = elf_sack.chars.each_slice(elf_sack.length / 2)
+            .map { |compartment| Set.new(compartment) }
+
+        # Use intersection for find chars in common
+        common_item = compartment1.intersection(compartment2).first
+        alpabet_map.index(common_item) + 1
+    end
+
+pp "Better Solution: Priority total for all sacks: #{priority_score}"
