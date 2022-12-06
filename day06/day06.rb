215a307087c9235d6d6ad5607934dc880959fcbd
Tommy Dugger <tommy.dugger78@gmail.com>
2022-12-05 22:51:58 -0700

    day06

---
+++ day06/day06.rb
@@ -0,0 +1,28 @@
+# ei_one: first marker after character 5
+# ei_two: first marker after character 6
+# ei_three: first marker after character 10
+# ei_four: first marker after character 11
+require 'set'
+
+def input
+    File.read("input")
+end
+
+def starting_position(number_of_unique_characters)
+    starts_at = 0
+    number_with_offset = number_of_unique_characters - 1
+    input.chars.each_with_index do |char, index|
+        if index >= number_with_offset
+            last_four = Set.new(input[index - number_with_offset..index].split(""))
+            if last_four.size == number_of_unique_characters
+                starts_at = index + 1
+                break
+            end
+        end
+    end
+
+    starts_at
+end
+
+pp "The marker starts at #{starting_position(4)} for part 1"
+pp "The marker starts at #{starting_position(14)} for part 2"
