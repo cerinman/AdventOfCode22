f011c2c94d1aac05b083a699a10ffaf84fe64de0
Tommy Dugger <tommy.dugger78@gmail.com>
2022-12-07 14:35:12 -0700

    day 7 solution

---
+++ day07/Directory.rb
@@ -0,0 +1,60 @@
+class Directory
+    attr_accessor :directory_size, :files, :sub_directories
+
+    def initialize
+        @directory_size = 0
+        @files = []
+        @sub_directories = {}
+    end
+    
+    def add_file(name, size)
+        @directory_size += size
+        @files << name
+    end
+
+    def add_sub_directory(name)
+        @sub_directories[name] = Directory.new
+    end
+
+    def get_sub_directory_by_name(name)
+        sub_directories[name]
+    end
+
+    def get_directory_size
+        directory_size + sum_sub_directories
+    end
+
+    def sum_sub_directories()
+        sub_directories.values.sum do |dir|
+            dir.get_directory_size
+        end
+    end
+
+    def sum_directories_within_threshold
+        threshold = 100000
+        sum = 0
+        sum += get_directory_size if get_directory_size <= threshold
+        sum += sub_directories.values.sum do |dir|
+            dir.sum_directories_within_threshold
+        end
+        sum
+    end
+
+    def get_directory_sizes(needed_to_update)
+        directories = []
+        directories << get_directory_size if get_directory_size > needed_to_update
+        sub_directories.values.each do |dir|
+            directories.concat(dir.get_directory_sizes(needed_to_update))
+        end
+        directories.sort
+    end
+
+    def find_directory_to_delete
+        total = 70000000
+        space_used = get_directory_size
+        available = 70000000 - space_used
+        needed_to_update = 30000000
+        options = get_directory_sizes(needed_to_update - available)
+        options.first
+    end
+end
---
+++ day07/day07.rb
@@ -0,0 +1,57 @@
+require_relative "Directory"
+
+class DaySeven
+    def initialize(input_path)
+        @root_directory = Directory.new
+        @directories = [@root_directory]
+        @input_path = input_path
+        parse_input
+    end
+
+    def sum_small_directories
+        @root_directory.sum_directories_within_threshold
+    end
+
+    def size_of_directory_to_delete
+        @root_directory.find_directory_to_delete
+    end
+
+    def read_input
+        File.readlines(@input_path).map(&:strip)
+    end
+
+    def parse_input
+        read_input.each do |line|
+            next if line.include?("$ ls")
+            parse_line(line)
+        end
+    end
+
+    def parse_command(command_info)
+        case command_info[2]
+        when ".."
+            @directories.pop
+        else
+            return if command_info[2] == "/"
+            @directories << @directories.last.get_sub_directory_by_name(command_info[2])
+        end
+    end
+
+    def parse_line(line)
+        split_line = line.split(" ")
+
+        case split_line[0]
+        when "$"
+            parse_command(split_line)
+        when "dir"
+            @directories.last.add_sub_directory(split_line[1])
+        else
+            @directories.last.add_file(split_line[1], split_line[0].to_i)
+        end
+    end
+end
+
+part_one = DaySeven.new("input")
+pp part_one.sum_small_directories
+part_two = DaySeven.new("input")
+pp part_two.size_of_directory_to_delete
